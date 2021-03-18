35145959bd57f2ec14de79fe029112464140e574
KrishnaBhosle2225 <kbhosle2000@gmail.com>
2021-03-19 02:26:34 +0530

    login and Registration using sharedpreference with validations

---
+++ app/src/main/java/com/krishna/login_registratioin_validation/MainActivity.java
@@ -0,0 +1,51 @@
+package com.krishna.login_registratioin_validation;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    SharedPreferences sharedPreferences;
+    public static final String fileName = "login";
+    public static final String USERNAME = "t1";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+
+        sharedPreferences = getSharedPreferences(fileName, Context.MODE_PRIVATE);
+        if(sharedPreferences.contains(USERNAME)){
+            textView.setText("Welcome To AsciiEducation");
+        }
+
+    }
+}
